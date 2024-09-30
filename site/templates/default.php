b2c6966b9ce843f0fce5c1b02899deedbe31e909
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2024-09-30 16:02:03 +0200

    update logo

--- site/templates/default.php
+++ site/templates/default.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.js"></script>
 <!-- <script src="./blockchain.js" defer></script> -->
-    
+<script src="https://kit.fontawesome.com/d4cbcb96c8.js" crossorigin="anonymous"></script>
 <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css" rel="stylesheet" type="text/css" />
 <script src="https://cdn.tailwindcss.com"></script>
     <?= js('assets/js/blockchain.js') ?>
@@ -17,7 +17,13 @@
 
 <body class="mt-0">
 <header class="header m-12 mt-8">
+<div class="midLogo">
     <a href="<?= $site->url() ?>" class="logo"><?= $site->title() ?></a>
+
+    <div class=" text-center">
+    <i class="fa-brands fa-bitcoin fa-10x"></i>    
+    </div>
+    
 
     <div class="flex md:flex-grow justify-end space-x-1">
         <?php foreach ($site->children()->listed() as $item): ?>
@@ -26,13 +32,15 @@
             </a>
         <?php endforeach; ?>
     </div>
+    </div>
+
 </header>
 
 
     <!-- Main Container -->
     <div class="container mx-auto">
         <div class="text-center">
-            <h1 class="text-4xl font-bold mt-8"><?= $page->title() ?></h1>
+            <h1 class="text-4xl font-bold"><?= $page->title() ?></h1>
             <p><?= $page->text() ?></p>
         </div>
 
